ddfbc6169015e0f3ce22d93f7a8673c952c27f7b
Malo <malo@msrt.ml>
2020-03-25 22:49:03 +0100

    Travis CI

--- src/main/java/fr/msrt/botgreffier/info/Info.java
+++ src/main/java/fr/msrt/botgreffier/info/Info.java
@@ -3,8 +3,8 @@ package fr.msrt.botgreffier.info;
 public final class Info {
 
     // Infos à éditer en fonction des versions du bot
-    public static final String VERSION = "4.6.0";
-    public static final String LAST_UPDATE = "22/03/2020 20:20";
-    public static final String INSTANCE_NAME = "Greffier, le retour";
+    public static final String VERSION = "4.6.1";
+    public static final String LAST_UPDATE = "25/03/2020 22:45";
+    public static final String INSTANCE_NAME = "VPS LS2k17";
 
 }
