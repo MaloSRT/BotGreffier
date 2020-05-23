8fb766659f5d2bc83a74063ca4f6b395f7d2bc5b
Malo <malo.serot@etudiant.univ-reims.fr>
2020-05-23 15:35:27 +0200

    màj JDA

--- src/main/java/fr/msrt/botgreffier/info/Info.java
+++ src/main/java/fr/msrt/botgreffier/info/Info.java
@@ -3,8 +3,8 @@ package fr.msrt.botgreffier.info;
 public final class Info {
 
     // Infos à éditer en fonction des versions du bot
-    public static final String VERSION = "4.10.1";
-    public static final String LAST_UPDATE = "11/05/2020 21:15";
+    public static final String VERSION = "4.10.2";
+    public static final String LAST_UPDATE = "23/05/2020 15:35";
     public static final String INSTANCE_NAME = "VPS LS2k17";
 
 }
