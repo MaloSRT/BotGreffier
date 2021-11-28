cce0ac97035bbc4e0c052b357ff9762834439a21
Malo <malo.serot@etudiant.univ-reims.fr>
2021-11-28 12:27:27 +0100

    Ajouts de données dans iadata.json

--- src/main/java/fr/msrt/botgreffier/info/Info.java
+++ src/main/java/fr/msrt/botgreffier/info/Info.java
@@ -3,8 +3,8 @@ package fr.msrt.botgreffier.info;
 public final class Info {
 
     // Infos à éditer en fonction des versions du bot
-    public static final String VERSION = "4.10.4";
-    public static final String LAST_UPDATE = "27/11/2021 20:52";
+    public static final String VERSION = "4.10.5";
+    public static final String LAST_UPDATE = "28/11/2021 12:20";
     public static final String INSTANCE_NAME = "Samsung";
 
 }
