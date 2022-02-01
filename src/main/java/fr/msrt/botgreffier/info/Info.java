db8077b2e48d1fb6c7fce2d6929103f0676e6423
Malo <malo.serot@etudiant.univ-reims.fr>
2022-02-01 22:08:57 +0100

    màj des dépendances

--- src/main/java/fr/msrt/botgreffier/info/Info.java
+++ src/main/java/fr/msrt/botgreffier/info/Info.java
@@ -4,7 +4,7 @@ public final class Info {
 
     // Infos à éditer en fonction des versions du bot
     public static final String VERSION = "4.10.6";
-    public static final String LAST_UPDATE = "23/01/2022 14:11";
+    public static final String LAST_UPDATE = "01/02/2022 22:07";
     public static final String INSTANCE_NAME = "Samsung";
 
 }
